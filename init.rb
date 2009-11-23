6fd9cb50c5d0fa7c999b9b801178c891f854a191
Jason Rush <jasonrush@jason-rushs-macbook-pro.local>
2009-11-22 19:18:53 -0700

    messing around a little

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), 'lib', 'common')
-Dir[File.join(JAR_FOLDER, '*.jar')].each do |jar|
+jar_folder = File.join(File.dirname(__FILE__), "..", "..", "jars")
+Dir[File.join(jar_folder, '*.jar')].each do |jar|
   require jar
 end
--- install.rb
+++ install.rb
@@ -1,3 +1,3 @@
-require File.join(File.dirname(__FILE__), 'lib', 'common')
+jar_folder = File.join(File.dirname(__FILE__), "..", "..", "jars")
 Dir.mkdir(jar_folder) unless File.exists?(jar_folder)
 raise "Uh oh. The jars folder seems to be a file" unless File.directory?(jar_folder)
