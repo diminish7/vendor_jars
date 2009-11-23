bd2b1e0bc63a1a4550b2aa01ce30f876ed0e1edd
Jason Rush <jasonrush@jason-rushs-macbook-pro.local>
2009-11-22 19:09:31 -0700

    moved common to lib

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), 'common')
+require File.join(File.dirname(__FILE__), 'lib', 'common')
 Dir[File.join(JAR_FOLDER, '*.jar')].each do |jar|
   require jar
 end
--- install.rb
+++ install.rb
@@ -1,3 +1,3 @@
-require File.join(File.dirname(__FILE__), 'common')
+require File.join(File.dirname(__FILE__), 'lib', 'common')
 Dir.mkdir(jar_folder) unless File.exists?(jar_folder)
 raise "Uh oh. The jars folder seems to be a file" unless File.directory?(jar_folder)
